14ffb44575652e2fcbd6220c23c40a1dd7d2b453
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-10-05 10:27:38 -0300

    agrego renderizado dinamico de cantidad de productos en resumen

--- src/components/Resumen/CardResumen.jsx
+++ src/components/Resumen/CardResumen.jsx
@@ -19,7 +19,7 @@ const CardResumen = ({ title, desc, quantity, img, price, pricesale }) => {
                 </div>
             </div>
             <div className="price-container">
-                <p>3U</p>
+                <p>{quantity}</p>
                 <span className="product-price">{formatPrice(totalPrice)}</span>
             </div>
         </div>

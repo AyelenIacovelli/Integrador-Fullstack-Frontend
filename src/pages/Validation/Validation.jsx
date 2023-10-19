513646674f15243b07fcbb4983e0e7345364d401
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-10-19 10:03:09 -0300

    prueba validation

--- src/pages/Validation/Validation.jsx
+++ src/pages/Validation/Validation.jsx
@@ -3,24 +3,35 @@ import React from 'react'
 // import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Button from '../../components/UI/Button/Button'
+import Helmet from "../../components/Helmet/Helmet"
+import CommonSection from "../../components/UI/common/CommonSection"
+import "./validation.css"
+import { useLocation } from 'react-router-dom';
 
 const Validation = () => {
     // const currentUser = useSelector(state => state.user.currentUser)
     const navigate = useNavigate()
 
-    return (
-        <div className='validation-container'>
+    const location = useLocation();
+    const username = location.state ? location.state.username : null;
 
-            <>
-                <h1>!BIENVENIDO/A! Te llegará un mail con un código de verificación</h1>
-                <Button onClick={() => {
-                    navigate('/validate')
-                }}>Validar tu cuenta</Button>
-                <Button onClick={() => {
-                    navigate('/')
-                }}>Validar más tarde</Button>
-            </>
-        </div>
+    return (
+        <Helmet title="Validation">
+            <CommonSection title="Validación de cuenta" />
+            <div className='validation-container'>
+
+                <h1 className='validation-h1'>¡BIENVENIDO/A {username ? `, ${username}` : ''}! Te llegará un mail con un código de verificación</h1>
+                <div className='validation-btn-container'>
+                    <Button onClick={() => {
+                        navigate('/validate')
+                    }}>Validar tu cuenta</Button>
+                    <Button onClick={() => {
+                        navigate('/')
+                    }}>Validar más tarde</Button>
+                </div>
+            </div>
+        </Helmet>
+
     )
 
 

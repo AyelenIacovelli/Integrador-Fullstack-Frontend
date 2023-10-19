de375f2fa9f9fffd3548676e7b163dbe89b0b51f
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-10-19 08:15:35 -0300

    load

--- src/components/UI/Loader/Loader.jsx
+++ src/components/UI/Loader/Loader.jsx
@@ -1,18 +1,15 @@
-// import React from 'react';
+import React from 'react';
 // import { motion, useTime, useTransform } from 'framer-motion';
-// import "./loader.css"
+import "./loader.css"
 
 
 
-const Loader = ()/*({ styles })*/ => {
+const Loader = () => {
     // const time = useTime();
     // const rotate = useTransform(time, [0, 1200], [0, 360], { clamp: false });
 
     return (
-        <div className='loader-container'>
-            {/* <motion.div args={styles} style={{ rotate }}></motion.div> */}
-            holis
-        </div>
+        <div class="loader"></div>
     );
 };
 

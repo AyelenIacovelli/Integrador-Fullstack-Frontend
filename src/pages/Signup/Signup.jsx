38454c1804078f7d7bd299d4ba4ffe1eb22a850e
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-10-19 09:20:15 -0300

    loader signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,27 +1,25 @@
+import React, { useState } from 'react';
+import Helmet from '../../components/Helmet/Helmet';
 import { Formik, Form } from 'formik';
-
-import LoginInput from "../../components/UI/LoginInput/LoginInput";
-import Submit from "../../components/UI/Submit/Submit";
-
+import LoginInput from '../../components/UI/LoginInput/LoginInput';
+import Submit from '../../components/UI/Submit/Submit';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import * as Yup from 'yup';
 import CommonSection from '../../components/UI/common/CommonSection';
-import Helmet from '../../components/Helmet/Helmet';
+import { createUser } from '../../axios/axios-user';
+import { useDispatch } from 'react-redux';
+import { setCurrentUser } from '../../redux/slices/userSlice';
+import useRedirect from '../../custom-hooks/useRedirect';
+import Loader from "../../components/UI/Loader/Loader"
 
-import { createUser } from "../../axios/axios-user";
-import { useDispatch } from "react-redux";
-import { setCurrentUser } from "../../redux/slices/userSlice";
-import useRedirect from "../../custom-hooks/useRedirect";
-
-import "./signup.css"
+import './signup.css';
 
 const Signup = () => {
     const dispatch = useDispatch();
-    // const navigate = useNavigate()
-    // const currentUser = useSelector(state => state.user.currentUser)
-    useRedirect("/validation")
+    const [loading, setLoading] = useState(false); // Estado para controlar el estado de carga
+    useRedirect('/validation');
 
     const validationSchema = Yup.object().shape({
         username: Yup.string().required('El nombre de usuario es requerido'),
@@ -29,42 +27,45 @@ const Signup = () => {
         password: Yup.string().min(6, 'La contraseña debe tener al menos 6 caracteres').required('La contraseña es requerida'),
     });
 
+    const handleSignup = async (values, actions) => {
+        try {
+            setLoading(true); // Activa el estado de carga
+
+            const user = await createUser(values.username, values.email, values.password);
+            actions.resetForm();
+
+            if (user) {
+                dispatch(setCurrentUser({
+                    ...user.usuario,
+                }));
+                toast.success('Usuario registrado');
+            }
+        } catch (error) {
+            toast.error(error.message);
+        } finally {
+            setLoading(false); // Desactiva el estado de carga después de completar el registro
+        }
+    };
 
     return (
         <Helmet title="Registrarse">
             <CommonSection title="Registrarse" />
             <section className="signup__section">
                 <div className="form__container">
-                    <h3 className='login-title'>Crea tu cuenta</h3>
+                    <h3 className="login-title">Crea tu cuenta</h3>
                     <Formik
                         initialValues={{
                             username: '',
                             email: '',
                             password: '',
-                            // file: null
                         }}
                         validationSchema={validationSchema}
-                        onSubmit={async (values, actions) => {
-                            try {
-                                const user = await createUser(values.username, values.email, values.password);
-                                actions.resetForm();
-                                if (user) {
-                                    dispatch(setCurrentUser({
-                                        ...user.usuario,
-                                    }));
-                                    toast.success('Usuario registrado');
-                                }
-                            } catch (error) {
-                                toast.error(error.message);
-                            }
-                        }}
+                        onSubmit={handleSignup}
                     >
                         <Form className="auth__form">
-                            <LoginInput name='username' type="text" placeholder="Nombre" />
-                            <LoginInput name='email' type="text" placeholder="Email" />
-                            <LoginInput name='password' type="password" placeholder="Password" />
-
-
+                            <LoginInput name="username" type="text" placeholder="Nombre" />
+                            <LoginInput name="email" type="text" placeholder="Email" />
+                            <LoginInput name="password" type="password" placeholder="Password" />
 
                             <Submit type="button" className="auth__btn">
                                 Crear cuenta
@@ -75,6 +76,7 @@ const Signup = () => {
                         </Form>
                     </Formik>
                 </div>
+                {loading && <Loader />} {/* Mostrar Loader si el estado de carga está activo */}
             </section>
         </Helmet>
     );

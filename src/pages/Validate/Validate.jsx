86bedacc575003a4dcc82d2afce084109c874c87
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-10-19 15:27:26 -0300

    validate p

--- src/pages/Validate/Validate.jsx
+++ src/pages/Validate/Validate.jsx
@@ -41,7 +41,6 @@ const Validate = () => {
                 currentUser?.verified ?
                     (<>
                         <h1>¡Tu cuenta ya fue verificada!</h1>
-                        <p>Recuerda que no podrás realizar compras en tanto no esté la cuenta validada</p>
                         <Button onClick={() => {
                             navigate('/')
                         }}>Volver al Home</Button>
@@ -49,6 +48,7 @@ const Validate = () => {
                     :
                     (<div className='validation-container'>
                         <h1>Revisa tu mail y validá tu cuenta</h1>
+                        <p>Recuerda que no podrás realizar compras en tanto no esté la cuenta validada</p>
                         <Formik
                             initialValues={{
                                 code: ''
